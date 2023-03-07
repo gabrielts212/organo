834f179e93db8f93e2f950447220576f0acb4a5c
gabrielts212 <gabrielts2122@gmail.com>
2023-03-07 16:17:28 -0300

    projetc

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import "./Banner.css"
+
+function Banner () {
+  return (
+<div  className="banner">
+
+<img src="images/banner.png" alt="O banner principal da página do Organo"/>
+</div>
+  )
+}
+
+export default Banner
---
+++ src/components/CampoTexto/CampoTexto.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./CampoTexto.css"
+const CampoTexto = () => {
+  return (
+    <div className="campo-texto">
+        CampoTexto
+    
+
+    <label>Nome</label>
+    <input placeholder='Digite o seu nome'/>
+    </div>
+  )
+}
+
+export default CampoTexto
